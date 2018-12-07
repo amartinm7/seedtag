071309ccb048b7a1cb3b81d1a8234a8ec1408642
amartinm7 <amartinm7@gmail.com>
2018-12-07 12:41:14 +0100

    updating service

--- src/main/java/org/amm/seedtag/service/ScanMessages.java
+++ src/main/java/org/amm/seedtag/service/ScanMessages.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ScanMessages {
-
     public Coordinates scan(RequestMessage requestMessage){
         return requestMessage.nextCoordinates();
     }
-
 }
