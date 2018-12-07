ba9194fcfa319f559fc58b26789b44a0c7c382b1
amartinm7 <amartinm7@gmail.com>
2018-12-07 10:54:59 +0100

    updating getDistance

--- src/main/java/org/amm/seedtag/model/protocol/Protocol.java
+++ src/main/java/org/amm/seedtag/model/protocol/Protocol.java
@@ -4,17 +4,16 @@ import org.amm.seedtag.model.message.Coordinates;
 import org.amm.seedtag.model.message.Scan;
 
 import java.awt.geom.Point2D;
-import java.time.DateTimeException;
-import java.time.ZoneId;
 import java.util.List;
 
 public interface Protocol {
+
     public List<Coordinates> execute (Scan[] scans);
 
-    static double getDistance (Coordinates coordinates) {
+    public static double getDistance (Coordinates coordinates) {
         final Point2D finalP = new Point2D.Double(0, 0);
         final Point2D point1 = new Point2D.Double(coordinates.getX(), coordinates.getY());
-        double distance = point1.distanceSq(finalP);
+        double distance = point1.distance(finalP);
         return distance;
     }
 }
